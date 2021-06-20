d3da7db6e6aac30a0b17a26de74ede1ad08f950b
Jesse Wallace <jesse.rudolf.wallace@gmail.com>
2021-06-20 14:38:29 +1000

    fixed typo

--- bio/picard/collectinsertsizemetrics/wrapper.py
+++ bio/picard/collectinsertsizemetrics/wrapper.py
@@ -16,7 +16,7 @@ java_opts = get_java_opts(snakemake)
 shell(
     "echo 'touching {snakemake.input} and {snakemake.output}' && "
     "touch {snakemake.input} && touch {snakemake.output.pdf} && "
-    "echo 'done. Running picard. && "
+    "echo 'done. Running picard.' && "
     "picard CollectInsertSizeMetrics {java_opts} {extra} "
     "INPUT={snakemake.input} "
     "OUTPUT={snakemake.output.txt} "
